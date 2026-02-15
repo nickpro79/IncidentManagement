2403b986ab00c432156723ec89ebda9e66f792d4
Nikhil <nikhilpraveen7777@gmail.com>
2026-02-15 22:29:42 +0530

    Added migrations

---
+++ IncidentMangement/IncidentManagement.Auth/Controllers/AuthController.cs
@@ -0,0 +1,40 @@
+﻿using IncidentManagement.Auth.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IncidentManagement.Auth.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthService _authService;
+
+        public AuthController(IAuthService authService)
+        {
+            _authService = authService;
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register(string username, string password)
+        {
+            var token = await _authService.Register(username, password);
+
+            if (token == null)
+                return BadRequest("User already exists");
+
+            return Ok(new { token });
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login(string username, string password)
+        {
+            var token = await _authService.Login(username, password);
+
+            if (token == null)
+                return Unauthorized();
+
+            return Ok(new { token });
+        }
+    }
+}
