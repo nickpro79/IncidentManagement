254f69066aa28b6ef3e40a069c9c8cef9bff3f70
LOQ <nikhilpraveen7777@gmail.com>
2026-02-28 22:07:30 +0530

    Role based tested.

--- IncidentMangement/IncidentManagement.Auth/Interfaces/IAuthService.cs
+++ IncidentMangement/IncidentManagement.Auth/Interfaces/IAuthService.cs
@@ -4,7 +4,7 @@ namespace IncidentManagement.Auth.Interfaces
 {
     public interface IAuthService
     {
-        Task<string?> Register(string username, string password);
+        Task<string?> Register(string username, string password, string? role);
         Task<string?> Login(string username, string password);
     }
 }
