cb3088713753421a00bdd702f971e1d5eddf260d
LOQ <nikhilpraveen7777@gmail.com>
2026-02-28 22:19:45 +0530

    Completed Ocelot project. Needs testing

--- IncidentMangement/IncidentManagement.Gateway/Program.cs
+++ IncidentMangement/IncidentManagement.Gateway/Program.cs
@@ -1,23 +1,14 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
 
-builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Services.AddOcelot();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
+await app.UseOcelot();
 
 app.Run();
