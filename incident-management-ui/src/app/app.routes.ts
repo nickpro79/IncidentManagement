8257eacb90c8f760cdbdda1fd87967b3d46d9b8a
LOQ <nikhilpraveen7777@gmail.com>
2026-03-06 20:19:09 +0530

    created navbar and sidebar

--- incident-management-ui/src/app/app.routes.ts
+++ incident-management-ui/src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
-import { Dashboard } from './pages/dashboard/dashboard'
+import { Dashboard } from './pages/dashboard/dashboard';
+import { Tickets } from './pages/tickets/tickets';
 
 export const routes: Routes = [
   { path: '', component: Dashboard },
+  { path: 'tickets', component: Tickets }
 ];
