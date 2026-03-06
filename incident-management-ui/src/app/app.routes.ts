6de8b48aa462ec1cfefc5119f1851418b3b27aa7
LOQ <nikhilpraveen7777@gmail.com>
2026-03-06 20:38:18 +0530

    added login and register page

--- incident-management-ui/src/app/app.routes.ts
+++ incident-management-ui/src/app/app.routes.ts
@@ -1,8 +1,15 @@
 import { Routes } from '@angular/router';
-import { Dashboard } from './pages/dashboard/dashboard';
+import { Dashboard} from './pages/dashboard/dashboard';
 import { Tickets } from './pages/tickets/tickets';
+import { Login } from './pages/login/login';
+import { Register } from './pages/register/register';
 
 export const routes: Routes = [
+
+  { path: 'login', component: Login },
+  { path: 'register', component: Register },
+
   { path: '', component: Dashboard },
   { path: 'tickets', component: Tickets }
+
 ];
