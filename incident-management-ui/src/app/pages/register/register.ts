24327fa189ead4ceeebc4bd15a9203372aa392a0
LOQ <nikhilpraveen7777@gmail.com>
2026-03-08 19:55:27 +0530

    added Auth service to angular

--- incident-management-ui/src/app/pages/register/register.ts
+++ incident-management-ui/src/app/pages/register/register.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
 import { ReactiveFormsModule, FormBuilder, Validators, FormGroup } from '@angular/forms';
+import { Auth } from '../../services/auth';
 
 @Component({
   selector: 'app-register',
@@ -15,7 +16,7 @@ export class Register{
   showPassword = false;
   registerForm!: FormGroup;
 
-  constructor(private fb: FormBuilder) {
+  constructor(private fb: FormBuilder,private authService: Auth) {
     this.registerForm = this.fb.group({
       username: ['', [Validators.required, Validators.minLength(3)]],
       email: ['', [Validators.required, Validators.email]],
@@ -27,10 +28,19 @@ export class Register{
     this.showPassword = !this.showPassword;
   }
 
-  onSubmit(){
-    if(this.registerForm.valid){
-      console.log(this.registerForm.value);
-    }
+onSubmit() {
+  if(this.registerForm.valid){
+    this.authService.register(this.registerForm.value).subscribe({
+      next: (res) => {
+        console.log('User registered', res);
+        alert("Registration successful");
+      },
+      error: (err) => {
+        console.error(err);
+        alert("Registration failed");
+      }
+    });
   }
+}
 
 }
---
+++ incident-management-ui/src/app/services/auth.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class Auth {
+
+  private apiUrl = 'http://localhost:7000/auth'; // backend URL
+
+  constructor(private http: HttpClient) {}
+
+  register(data: any): Observable<any> {
+    return this.http.post(`${this.apiUrl}/register`, data);
+  }
+
+  login(data: any): Observable<any> {
+    return this.http.post(`${this.apiUrl}/login`, data);
+  }
+
+}
